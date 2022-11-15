008f2c9c4765678962734fb9adf191582cbb0791
Claudia Roberta <2016crmm@gmail.com>
2022-11-15 20:57:12 -0300

    Criacao do Projeto

---
+++ backend/src/main/java/com/robertametas/crmeta/CrmetaApplication.java
@@ -0,0 +1,13 @@
+package com.robertametas.crmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/robertametas/crmeta/CrmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertametas.crmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
