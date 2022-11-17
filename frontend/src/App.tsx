0b52836b38da9f46b31f8b7bc6c3a88535fc4f97
Claudia Roberta <2016crmm@gmail.com>
2022-11-16 22:39:38 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import BtNotificacao from "./componentes/BtNotificacao"
 
 function App() {
   return (
+    <>
     <h1>Olá terráqueos!</h1>
-  )
+    <BtNotificacao/>
+    <BtNotificacao/>
+    <BtNotificacao/>
+    <BtNotificacao/>
+    </>
+    )
 }
 
 export default App
---
+++ frontend/src/componentes/BtNotificacao/index.tsx
@@ -0,0 +1,13 @@
+import icone from '../../assets/img/iconNotificacao.svg'
+import './styles.css'
+
+
+function BtNotificacao() {
+    return (
+        <div className="crmeta-red-btn">
+            <img src={icone} alt="Notificar"/>
+        </div>
+    )
+}
+
+export default BtNotificacao
