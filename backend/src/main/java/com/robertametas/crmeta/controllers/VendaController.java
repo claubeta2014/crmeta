3fc490835ca3f6b6dee4df83fc8624ac0c97778d
claubeta2014 <2016crmm@gmail.com>
2022-12-01 13:57:52 -0300

    SMS do Twilio

--- backend/src/main/java/com/robertametas/crmeta/controllers/VendaController.java
+++ backend/src/main/java/com/robertametas/crmeta/controllers/VendaController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robertametas.crmeta.entities.Venda;
+import com.robertametas.crmeta.services.SmsService;
 import com.robertametas.crmeta.services.VendaService;
 
 @RestController
@@ -18,6 +20,9 @@ public class VendaController {
 	@Autowired
 	private VendaService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Venda> findVendas(
 			@RequestParam(value= "minDate", defaultValue="") String minDate, 
@@ -25,6 +30,11 @@ public class VendaController {
 			Pageable pageable){
 		return service.findVendas(minDate, maxDate, pageable);
 		
+	}
+	
+	@GetMapping("/{id}/notificacao")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 	
 
